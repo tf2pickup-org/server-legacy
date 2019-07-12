568d8e346d9ca73d8a82e9d34b765ac6cb1025d3
Michał Garapich <michal@garapich.pl>
2019-07-13 00:07:39 +0200

    join & leave queue

--- src/queue/queue.ts
+++ src/queue/queue.ts
@@ -1,3 +1,4 @@
+import logger from '../logger';
 import { Queue } from './models/queue';
 
 export const queue: Queue = {
@@ -11,3 +12,27 @@ export const queue: Queue = {
   },
   players: [],
 };
+
+export function joinQueue(slot: string, playerId: string) {
+  if (!slot) {
+    throw new Error('slot undefined');
+  }
+
+  const gameClass = queue.config.classes.find(cls => cls.name === slot);
+  if (!gameClass) {
+    throw new Error('invalid slot');
+  }
+
+  const takenSlotsCount = queue.players.filter(p => p.slot === slot).length;
+  if (takenSlotsCount >= gameClass.count) {
+    throw new Error('slot unavailable');
+  }
+
+  queue.players = [ ...queue.players.filter(p => p.playerId !== playerId), { slot, playerId } ];
+  logger.debug(`${playerId} joined the queue as ${slot}`);
+}
+
+export function leaveQueue(playerId: string) {
+  queue.players = [ ...queue.players.filter(p => p.playerId !== playerId) ];
+  logger.debug(`${playerId} left the queue`);
+}
--- src/queue/routes.ts
+++ src/queue/routes.ts
@@ -1,10 +1,31 @@
 import { Router } from 'express';
-import { queue } from './queue';
+import { ensureAuthenticated } from '../auth';
+import { joinQueue, leaveQueue, queue } from './queue';
 
 const router = Router();
 
 router
   .route('/')
   .get((req, res) => res.status(200).send(queue));
+
+router
+  .route('/players')
+  .get((req, res) => res.status(200).send(queue.players))
+  .post(ensureAuthenticated, (req, res) => {
+    const playerId = req.user.id;
+    const slot = req.body.slot;
+
+    try {
+      joinQueue(slot, playerId);
+      return res.status(200).send(queue.players);
+    } catch (error) {
+      return res.status(400).send({ message: error.message });
+    }
+  })
+  .delete(ensureAuthenticated, (req, res) => {
+    const playerId = req.user.id;
+    leaveQueue(playerId);
+    return res.status(200).send(queue.players);
+  });
 
 export default router;
