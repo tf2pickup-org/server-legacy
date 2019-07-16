d792ae960fc731d4673e0db016b1a969f03975b0
Michał Garapich <michal@garapich.pl>
2019-07-16 12:45:26 +0200

    remove server impl

--- src/game-servers/routes.ts
+++ src/game-servers/routes.ts
@@ -1,9 +1,23 @@
 import { Router } from 'express';
-import { Document } from 'mongoose';
+import { Document, Types } from 'mongoose';
 import { ensureAuthenticated, ensureRole } from '../auth';
+import logger from '../logger';
+import { IPlayer } from '../players/models/player';
 import { renameId } from '../utils';
 import { gameServerController } from './game-server-controller';
 import { GameServer, IGameServer } from './models/game-server';
+
+function removeRcon(user: IPlayer): (doc: Document, ret: any) => any {
+  return (doc: Document, ret: any) => {
+    ret = renameId(doc, ret);
+
+    if (!user || !user.role) {
+      delete ret.rconPassword;
+    }
+
+    return ret;
+  };
+}
 
 const router = Router();
 
@@ -11,15 +25,7 @@ router
   .route('/')
   .get(async (req, res) => {
     const servers = await GameServer.find();
-    return res.status(200).send(servers.map(s => s.toJSON({ transform: (doc: Document, ret: any) => {
-      ret = renameId(doc, ret);
-
-      if (!req.user || !req.user.role) {
-        delete ret.rconPassword;
-      }
-
-      return ret;
-    }})));
+    return res.status(200).send(servers.map(s => s.toJSON({ transform: removeRcon(req.user) })));
   })
   .post(ensureAuthenticated, ensureRole('super-user'), async (req, res) => {
     const gameServer = req.body as IGameServer;
@@ -32,6 +38,36 @@ router
       return res.status(201).send(ret.toJSON());
     } catch (error) {
       return res.status(400).send({ message: error.message });
+    }
+  });
+
+router
+  .route('/:id')
+  .get(async (req, res) => {
+    const id = req.params.id;
+    if (!Types.ObjectId.isValid(id)) {
+      return res.status(400).send({ message: 'invalid id' });
+    }
+
+    const server = await GameServer.findById(id);
+    if (server) {
+      return res.status(200).send(server.toJSON({ transform: removeRcon(req.user) }));
+    } else {
+      return res.status(404).send({ message: 'no such game server' });
+    }
+  })
+  .delete(ensureAuthenticated, ensureRole('super-user'), async (req, res) => {
+    const id = req.params.id;
+    if (!Types.ObjectId.isValid(id)) {
+      return res.status(400).send({ message: 'invalid id' });
+    }
+
+    const { ok } = await GameServer.deleteOne({ _id: id });
+    logger.debug('game server removed');
+    if (ok) {
+      return res.status(204).send();
+    } else {
+      return res.status(500).send({ message: 'unabled to remove server' });
     }
   });
 
