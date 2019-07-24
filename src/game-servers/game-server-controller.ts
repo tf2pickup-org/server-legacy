22f9ea4bff8482fb7ae69aa6cb98060c748e2ca4
Michał Garapich <michal@garapich.pl>
2019-07-25 01:10:48 +0200

    cleanup the server

--- src/game-servers/game-server-controller.ts
+++ src/game-servers/game-server-controller.ts
@@ -18,6 +18,7 @@ const resolve = promisify(resolveCb);
 
 class GameServerController {
 
+  private logAddress = `${config.log_relay.address}:${config.log_relay.port}`;
   private gameEventListener = new GameEventListener();
 
   constructor() {
@@ -26,7 +27,8 @@ class GameServerController {
 
     this.gameEventListener.on('match start', async ({ source }) => {
       try {
-        const game = await this.getGame(source);
+        const server = await this.getSourceServer(source);
+        const game = await this.getAssignedGame(server);
         gameController.onMatchStarted(game);
       } catch (error) {
         logger.error(error.message);
@@ -35,8 +37,10 @@ class GameServerController {
 
     this.gameEventListener.on('match end', async ({ source }) => {
       try {
-        const game = await this.getGame(source);
+        const server = await this.getSourceServer(source);
+        const game = await this.getAssignedGame(server);
         gameController.onMatchEnded(game);
+        this.cleanup(server);
       } catch (error) {
         logger.error(error.message);
       }
@@ -79,41 +83,59 @@ class GameServerController {
 
   public async configure(queueConfig: QueueConfig, server: IGameServer, game: IGame): Promise<ServerInfoForPlayer> {
     logger.info(`configuring server ${server.name}...`);
+    logger.debug(`[${server.address}:${server.port}] using rcon password ${server.rconPassword}`);
+    try {
+      const rcon = new Rcon({ packetResponseTimeout: 30000 });
+      await rcon.connect({
+        host: server.address,
+        port: server.port,
+        password: server.rconPassword,
+      });
 
-    logger.debug(`using password ${server.rconPassword} for game server ${server.address}:${server.port}`);
-    const rcon = new Rcon({ packetResponseTimeout: 30000 });
-    await rcon.connect({
-      host: server.address,
-      port: server.port,
-      password: server.rconPassword,
-    });
+      logger.debug(`[${server.name}] adding log address ${this.logAddress}...`);
+      await rcon.send(`logaddress_add ${this.logAddress}`);
 
-    const logAddress = `${config.log_relay.address}:${config.log_relay.port}`;
-    logger.debug(`server ${server.name}: adding log address ${logAddress}...`);
-    await rcon.send(`logaddress_add ${logAddress}`);
+      logger.debug(`[${server.name}] kicking all players...`);
+      await rcon.send(`kickall`);
+      logger.debug(`[${server.name}] changing map to ${game.map}...`);
+      await rcon.send(`changelevel ${game.map}`);
 
-    logger.debug(`server ${server.name}: kicking all players...`);
-    await rcon.send(`kickall`);
-    logger.debug(`server ${server.name}: changing map to ${game.map}...`);
-    await rcon.send(`changelevel ${game.map}`);
+      for (const execConfig of queueConfig.execConfigs) {
+        logger.debug(`[${server.name}] executing ${execConfig}...`);
+        await rcon.send(`exec ${execConfig}`);
+      }
 
-    for (const execConfig of queueConfig.execConfigs) {
-      logger.debug(`server ${server.name}: executing ${execConfig}...`);
-      await rcon.send(`exec ${execConfig}`);
+      const password = generator.generate({ length: 10, numbers: true, uppercase: true });
+      logger.debug(`[${server.name}] settings password to ${password}...`);
+      await rcon.send(`sv_password ${password}`);
+      await rcon.end();
+      logger.info(`done with configuring server ${server.name}`);
+
+      const connectString = `connect ${server.address}:${server.port}; password ${password}`;
+      logger.info(`${server.name} connect: ${connectString}`);
+
+      return {
+        connectString,
+      };
+    } catch (error) {
+      throw new Error(`could not configure server ${server.name} (${error.message})`);
     }
+  }
 
-    const password = generator.generate({ length: 10, numbers: true, uppercase: true });
-    logger.debug(`server ${server.name}: settings password to ${password}...`);
-    await rcon.send(`sv_password ${password}`);
-    await rcon.end();
-    logger.info(`done with configuring server ${server.name}`);
+  private async cleanup(server: IGameServer) {
+    try {
+      const rcon = new Rcon({ packetResponseTimeout: 30000 });
+      await rcon.connect({
+        host: server.address,
+        port: server.port,
+        password: server.rconPassword,
+      });
 
-    const connectString = `connect ${server.address}:${server.port}; password ${password}`;
-    logger.info(`${server.name} connect: ${connectString}`);
-
-    return {
-      connectString,
-    };
+      logger.debug(`[${server.name}] removing log address ${this.logAddress}...`);
+      await rcon.send(`logaddress_del ${this.logAddress}`);
+    } catch (error) {
+      throw new Error(`could not cleanup server ${server.name} (${error.message})`);
+    }
   }
 
   private async checkAllServers() {
@@ -125,24 +147,31 @@ class GameServerController {
     }
   }
 
-  private async getGame(source: GameEventSource): Promise<IGame> {
+  private async getSourceServer(source: GameEventSource): Promise<IGameServer> {
     const server = await GameServer.findOne({
       resolvedIpAddresses: source.address,
       port: source.port,
     });
     if (server) {
-      const assignment = await GameServerAssignment
-        .find({ server: server.id })
-        .find({ $or: [{ 'game.state': 'started' }, { 'game.state': 'launching' }]})
-        .findOne();
-
-      if (assignment) {
-        return assignment.game;
-      } else {
-        throw new Error(`no game assigned for server ${server.name}`);
-      }
+      return server;
     } else {
       throw new Error(`no such server: ${source.address}:${source.port}`);
+    }
+  }
+
+  /**
+   * Returns an ongoing game that takes place on the given server.
+   */
+  private async getAssignedGame(server: IGameServer): Promise<IGame> {
+    const assignment = await GameServerAssignment
+      .find({ server })
+      .find({ $or: [{ 'game.state': 'started' }, { 'game.state': 'launching' }]})
+      .findOne();
+
+    if (assignment) {
+      return assignment.game;
+    } else {
+      throw new Error(`no game assigned for server ${server.name}`);
     }
   }
 
