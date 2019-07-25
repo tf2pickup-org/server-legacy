c3b5c9f54f5f0d013395f039415cbf2f2b5931ff
Michał Garapich <michal@garapich.pl>
2019-07-25 04:10:24 +0200

    ensureRole: accept multiple roles

--- src/auth/ensure-role.ts
+++ src/auth/ensure-role.ts
@@ -1,9 +1,9 @@
 import { NextFunction, Request, Response } from 'express';
 import { PlayerRole } from 'players/models/player-role';
 
-export function ensureRole(role: PlayerRole): (req: Request, res: Response, next: NextFunction) => void {
+export function ensureRole(...roles: PlayerRole[]): (req: Request, res: Response, next: NextFunction) => void {
   return (req: Request, res: Response, next: NextFunction) => {
-    if (req.user.role !== role) {
+    if (!roles.includes(req.user.role)) {
       res.status(403).send('Forbidden');
     } else {
       next();
