22a9130bd78e2d53329b3c86acf2968959cf4da4
Michał Garapich <michal@garapich.pl>
2019-07-25 03:30:00 +0200

    make etf2l account requirement optional

--- src/auth/steam.ts
+++ src/auth/steam.ts
@@ -17,21 +17,25 @@ passport.use(new steam.Strategy({
   let player = await Player.findOne({ steamId: profile.id });
 
   if (!player) {
+    let name = profile.displayName;
     let etf2lProfile: Etf2lPlayer;
     try {
       etf2lProfile = await fetchEtf2lPlayerInfo(profile.id);
+      name = etf2lProfile.name;
     } catch (error) {
-      return done('no etf2l profile', null);
+      if (config.requireEtf2lAccount) {
+        return done('no etf2l profile', null);
+      }
     }
 
     player = await new Player({
       steamId: profile.id,
-      name: etf2lProfile.name,
+      name,
       avatarUrl: profile.photos[0].value,
       role: config.super_user === profile.id ? 'super-user' : null,
       hasAcceptedRules: false,
     }).save();
-    logger.info(`new user: ${etf2lProfile.name} (steamId: ${profile.id})`);
+    logger.info(`new user: ${name} (steamId: ${profile.id})`);
   } else {
     player.avatarUrl = profile.photos[0].value;
     await player.save();
