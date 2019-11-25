747cd2e0b36372ade9963c4369cb0fba8115fc70
Michał <michal@garapich.pl>
2019-11-25 17:38:37 +0100

    pickTeams(): add TeamOverrides

--- src/games/utils/pick-teams.ts
+++ src/games/utils/pick-teams.ts
@@ -7,10 +7,32 @@ export interface PlayerSlot {
   skill: number; // the skill for the given gameClass
 }
 
+export interface TeamOverrides {
+  friends: string[][];
+}
+
+interface InterimTeamSetup {
+  [teamId: number]: PlayerSlot[];
+  skillDifference: number;
+}
+
+function filterTeamOverrides(teamSetups: InterimTeamSetup[], overrides: TeamOverrides): InterimTeamSetup[] {
+  return teamSetups.filter(setup => {
+    let hasAllFriends = false;
+    overrides.friends.forEach(friends => {
+      if (friends.every(f => !!setup[0].find(s => s.playerId === f))) {
+        hasAllFriends = true;
+      }
+    });
+
+    return hasAllFriends;
+  });
+}
+
 /**
  * From the given pool of players make two teams that make the smallest average skill difference.
  */
-export function pickTeams(players: PlayerSlot[], gameClasses: string[]): GamePlayer[] {
+export function pickTeams(players: PlayerSlot[], gameClasses: string[], overrides?: TeamOverrides): GamePlayer[] {
   const allPossibilities: Array<{
     gameClass: string,
     allClassCombinations: Array<{ [teamId: number]: PlayerSlot[] }>,
@@ -73,10 +95,7 @@ export function pickTeams(players: PlayerSlot[], gameClasses: string[]): GamePla
 
   makeAllCombinations([]);
 
-  const allCombinations: Array<{
-    [teamId: number]: PlayerSlot[],
-    skillDifference: number,
-  }> = tmp.map(c => c.reduce((prev, curr) => {
+  let allCombinations: InterimTeamSetup[] = tmp.map(c => c.reduce((prev, curr) => {
     prev[0] = prev[0].concat(curr[0]);
     prev[1] = prev[1].concat(curr[1]);
     return prev;
@@ -88,13 +107,19 @@ export function pickTeams(players: PlayerSlot[], gameClasses: string[]): GamePla
     c.skillDifference = Math.abs(skillRed - skillBlu);
   });
 
+  logger.debug(`${allCombinations.length} possible team setups`);
+
+  if (overrides) {
+    allCombinations = filterTeamOverrides(allCombinations, overrides);
+    logger.debug(`${allCombinations.length} possible team setups after filtering out overrides`);
+  }
+
   allCombinations.sort((a, b) => a.skillDifference - b.skillDifference);
   const lowestSkillDifference = allCombinations[0].skillDifference;
   const possibleTeams = allCombinations.filter(c => c.skillDifference === lowestSkillDifference);
   logger.debug(`there are ${possibleTeams.length} possible teams with lowest skill difference`);
 
   const selected = possibleTeams[Math.floor(Math.random() * possibleTeams.length)];
-
   logger.info(`team average skill difference = ${selected.skillDifference}`);
 
   return Object.keys(selected)
