1a5a20f4beafff686309a5ec78cd3f1b71014c68
Michał Garapich <michal@garapich.pl>
2019-12-07 03:03:33 +0100

    DiscordBotService: handle no discord

--- src/discord/services/discord-bot-service.ts
+++ src/discord/services/discord-bot-service.ts
@@ -10,6 +10,7 @@ import { PlayerBan } from '../../players/models/player-ban';
 export class DiscordBotService {
 
   private client = new discord.Client();
+  private enabled = false;
 
   constructor(
     @inject('config') private config: Config,
@@ -17,6 +18,7 @@ export class DiscordBotService {
     if (this.config.discord) {
       this.client.on('ready', () => {
         logger.info(`Discord: logged in as ${this.client.user.tag}`);
+        this.enabled = true;
       });
 
       this.client.login(this.config.discord.token)
@@ -25,39 +27,35 @@ export class DiscordBotService {
   }
 
   public notifyQueue(currentPlayerCount: number, targetPlayerCount: number) {
-    if (!this.client.channels) {
-      return;
-    }
-
-    const channel = this.client.channels.get(this.config.discord.channels.queueNotifications) as discord.TextChannel;
-    if (channel) {
-      channel.send(`<@&610855230992678922> ${currentPlayerCount}/${targetPlayerCount} in the queue.
-      Go to ${this.config.clientUrl} and don't miss the next game!`);
-    } else {
-      logger.warn(`channel id ${this.config.discord.channels.queueNotifications} not found`);
+    if (this.enabled) {
+      const channel = this.client.channels.get(this.config.discord.channels.queueNotifications) as discord.TextChannel;
+      if (channel) {
+        channel.send(`<@&610855230992678922> ${currentPlayerCount}/${targetPlayerCount} in the queue.
+        Go to ${this.config.clientUrl} and don't miss the next game!`);
+      } else {
+        logger.warn(`channel id ${this.config.discord.channels.queueNotifications} not found`);
+      }
     }
   }
 
   public async notifyBan(ban: PlayerBan) {
-    if (!this.client.channels) {
-      return;
-    }
+    if (this.enabled) {
+      const channelId = this.config.discord.channels.banNotifications;
+      const channel = this.client.channels.get(channelId) as discord.TextChannel;
+      if (channel) {
+        const admin = await playerModel.findById(ban.admin);
+        const player = await playerModel.findById(ban.player);
 
-    const channelId = this.config.discord.channels.banNotifications;
-    const channel = this.client.channels.get(channelId) as discord.TextChannel;
-    if (channel) {
-      const admin = await playerModel.findById(ban.admin);
-      const player = await playerModel.findById(ban.player);
+        const embed = new discord.RichEmbed()
+          .setColor('#dc3545')
+          .setTitle('Ban added')
+          .addField('Admin', admin.name)
+          .addField('Player', player.name)
+          .addField('Reason', ban.reason)
+          .setTimestamp();
 
-      const embed = new discord.RichEmbed()
-        .setColor('#dc3545')
-        .setTitle('Ban added')
-        .addField('Admin', admin.name)
-        .addField('Player', player.name)
-        .addField('Reason', ban.reason)
-        .setTimestamp();
-
-      channel.send(embed);
+        channel.send(embed);
+      }
     }
   }
 }
